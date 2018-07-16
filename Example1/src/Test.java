7b7dde93f8bcde4ca6ac7d4d279910e993bae7e9
saikiran <aakulasaikiran>
2018-07-16 18:15:59 +0530

    commit4

--- Example1/src/Test.java
+++ Example1/src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		String[] s = { "apple", "banana", "mongo", "bat" };
+		String[] s = { "apple", "banana", "mongo", "bat","sai" };
 
 		for (int i = 0; i < s.length; i++) {
 			System.out.println(s[i] + " ");
