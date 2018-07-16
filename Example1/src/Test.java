8a3364f6102f95dffd67b12a65ebd1530a770c9a
saikiran <aakulasaikiran>
2018-07-16 18:24:01 +0530

    commit6

--- Example1/src/Test.java
+++ Example1/src/Test.java
@@ -2,11 +2,12 @@
 public class Test {
 
 	public static void main(String[] args) {
-	String[] s= {"apple","banana","mongo"};
+	String[] s= {"apple","banana","mongo","PineApple"};
 
 	
 	for(int i=0;i<s.length;i++) {
 		System.out.println(s[i]+" ");
+		System.out.println("How are You :");
 	}
 	
 	
